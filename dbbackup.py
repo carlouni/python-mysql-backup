6687121b4933cb52f4281d9c1d0a4f1823fd095b
Gonzalo Carlo, Cardenas Magno <gcmuni@gmail.com>
2017-01-31 12:31:30 +1000

    Fix issue when the backup path has blank spaces or special characters. It now compresses the sqll file result into a .tar.gz file.

--- dbbackup.py
+++ dbbackup.py
@@ -2,14 +2,14 @@
 ###########################################################
 #
 # This python script is used for mysql database backup
-# using mysqldump utility.
+# using mysqldump and tar utility.
 #
 # Written by : Rahul Kumar
 # Website: http://tecadmin.net
 # Created date: Dec 03, 2013
-# Last modified: Dec 03, 2013
-# Tested with : Python 2.6.6
-# Script Revision: 1.1
+# Last modified: jan 31, 2017 by Gonzalo Cardenas
+# Tested with : Python 2.7.12
+# Script Revision: 1.2
 #
 ##########################################################
 
@@ -17,26 +17,28 @@
 import os
 import time
 import datetime
+import pipes
 
 # MySQL database details to which backup to be done. Make sure below user having enough privileges to take databases backup.
 # To take multiple databases backup, create any file like /backup/dbnames.txt and put databses names one on each line and assignd to DB_NAME variable.
 
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_USER_PASSWORD = '_root_user_password_'
+DB_HOST = 'your_mysql_host' #ie. localhost
+DB_USER = 'your_user_name'
+DB_USER_PASSWORD = 'your_password'
 #DB_NAME = '/backup/dbnames.txt'
-DB_NAME = 'db_name'
-BACKUP_PATH = '/backup/dbbackup/'
+DB_NAME = 'your_db_name'
+BACKUP_PATH = '/your/backup/folder/path/'
 
 # Getting current datetime to create seprate backup folder like "12012013-071334".
-DATETIME = time.strftime('%m%d%Y-%H%M%S')
+DATETIME = time.strftime('%Y%m%d-%H%M%S')
 
 TODAYBACKUPPATH = BACKUP_PATH + DATETIME
 
 # Checking if backup folder already exists or not. If not exists will create it.
-print "creating backup folder"
-if not os.path.exists(TODAYBACKUPPATH):
-    os.makedirs(TODAYBACKUPPATH)
+try:
+    os.stat(TODAYBACKUPPATH)
+except:
+    os.mkdir(TODAYBACKUPPATH)
 
 # Code for checking if you want to take single database backup or assinged multiple backups in DB_NAME.
 print "checking for databases names file."
@@ -61,14 +63,22 @@ if multi:
    while p <= flength:
        db = dbfile.readline()   # reading database name from file
        db = db[:-1]         # deletes extra line
-       dumpcmd = "mysqldump -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + TODAYBACKUPPATH + "/" + db + ".sql"
+       dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
        os.system(dumpcmd)
+       compcmd = "tar -zcvf " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".tar.gz " +  pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
+       os.system(compcmd)
+       delcmd = "rm " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
+       os.system(delcmd)
        p = p + 1
    dbfile.close()
 else:
    db = DB_NAME
-   dumpcmd = "mysqldump -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + TODAYBACKUPPATH + "/" + db + ".sql"
+   dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
    os.system(dumpcmd)
+   compcmd = "tar -zcvf " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".tar.gz " +  pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
+   os.system(compcmd)
+   delcmd = "rm " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
+   os.system(delcmd)
 
 print "Backup script completed"
 print "Your backups has been created in '" + TODAYBACKUPPATH + "' directory"
