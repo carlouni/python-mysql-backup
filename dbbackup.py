8e141f5b96a0bb8b10524df26a4edba364879815
Gonzalo Cardenas <gcmuni@gmail.com>
2020-04-22 13:58:56 +1000

    feat/database_json_list accept list of DBs in a file with JSON format. File is passed as a parameter from command line

--- dbbackup.py
+++ dbbackup.py
@@ -4,81 +4,82 @@
 # This python script is used for mysql database backup
 # using mysqldump and tar utility.
 #
-# Written by : Rahul Kumar
+# Written by :  Gonzalo Cardenas
+# Last modified: Apr 22, 2020
+# Tested with : Python 2.7.17
+# Script Revision: 1.3
+#
+# Original version: Rahul Kumar
 # Website: http://tecadmin.net
 # Created date: Dec 03, 2013
-# Last modified: jan 31, 2017 by Gonzalo Cardenas
-# Tested with : Python 2.7.12
-# Script Revision: 1.2
 #
 ##########################################################
 
 # Import required python libraries
 import os
 import time
-import datetime
 import pipes
+import json
+import sys
+from subprocess import Popen, PIPE
 
 # MySQL database details to which backup to be done. Make sure below user having enough privileges to take databases backup.
-# To take multiple databases backup, create any file like /backup/dbnames.txt and put databses names one on each line and assignd to DB_NAME variable.
-
-DB_HOST = 'your_mysql_host' #ie. localhost
-DB_USER = 'your_user_name'
-DB_USER_PASSWORD = 'your_password'
-#DB_NAME = '/backup/dbnames.txt'
-DB_NAME = 'your_db_name'
-BACKUP_PATH = '/your/backup/folder/path/'
+# Pass the path of the file containing the list of DBs in JSON format
+# Examples:
+# $ python /path/to/dbbackup.py /path/to/databases.json
+# $ /path/to/dbbackup.py /path/to/databases.json
+db_list = []
+try:
+    json_file = open(pipes.quote(sys.argv[1]))
+    db_list = json.load(json_file)
+except Exception as e:
+    sys.stderr.write("Error reading file: " + sys.argv[1] + "\n")
+    sys.stderr.write(str(e) + "\n")
+    sys.exit(1)
 
 # Getting current datetime to create seprate backup folder like "12012013-071334".
 DATETIME = time.strftime('%Y%m%d-%H%M%S')
 
-TODAYBACKUPPATH = BACKUP_PATH + DATETIME
+result_list =[]
+for db in db_list:
+    today_bkp_path = db["bkpPath"] + DATETIME
 
-# Checking if backup folder already exists or not. If not exists will create it.
-try:
-    os.stat(TODAYBACKUPPATH)
-except:
-    os.mkdir(TODAYBACKUPPATH)
+    # Checking if backup folder already exists or not. If not exists will create it.
+    try:
+        os.stat(pipes.quote(today_bkp_path))
+    except:
+        os.mkdir(pipes.quote(today_bkp_path))
 
-# Code for checking if you want to take single database backup or assinged multiple backups in DB_NAME.
-print "checking for databases names file."
-if os.path.exists(DB_NAME):
-    file1 = open(DB_NAME)
-    multi = 1
-    print "Databases file found..."
-    print "Starting backup of all dbs listed in file " + DB_NAME
-else:
-    print "Databases file not found..."
-    print "Starting backup of database " + DB_NAME
-    multi = 0
+    try:
+        print("Backing up DB: " + db["dbName"])
 
-# Starting actual database backup process.
-if multi:
-   in_file = open(DB_NAME,"r")
-   flength = len(in_file.readlines())
-   in_file.close()
-   p = 1
-   dbfile = open(DB_NAME,"r")
+        # Dump database
+        p = Popen(["mysqldump", "--login-path=" + db["loginPath"], db["dbName"]], stdout=PIPE, stderr=PIPE)
+        out, err = p.communicate()
+        if len(err) > 0:
+            raise Exception(err)
+        f = open(pipes.quote(today_bkp_path) + "/" + db["dbName"] + ".sql", "w")
+        f.write(out)
+        f.close()
 
-   while p <= flength:
-       db = dbfile.readline()   # reading database name from file
-       db = db[:-1]         # deletes extra line
-       dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-       os.system(dumpcmd)
-       compcmd = "tar -zcvf " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".tar.gz " +  pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-       os.system(compcmd)
-       delcmd = "rm " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-       os.system(delcmd)
-       p = p + 1
-   dbfile.close()
-else:
-   db = DB_NAME
-   dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-   os.system(dumpcmd)
-   compcmd = "tar -zcvf " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".tar.gz " +  pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-   os.system(compcmd)
-   delcmd = "rm " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-   os.system(delcmd)
+        # Compress sql file
+        compcmd = "tar -zcvf " + pipes.quote(today_bkp_path) + "/" + db["dbName"] + ".tar.gz " +  pipes.quote(today_bkp_path) + "/" + db["dbName"] + ".sql"
+        os.system(compcmd)
+        delcmd = "rm " + pipes.quote(today_bkp_path) + "/" + db["dbName"] + ".sql"
+        os.system(delcmd)
 
-print "Backup script completed"
-print "Your backups has been created in '" + TODAYBACKUPPATH + "' directory"
+        print "Backup script completed"
+        print "Your backups has been created in '" + today_bkp_path + "' directory\n"
+        result_list.append({'db': db["dbName"], 'result': 'Succeeded'})
+    except Exception as e:
+        sys.stderr.write("Error backing up DB: " + db["dbName"] + "\n")
+        sys.stderr.write(str(e) + "\n")
+        result_list.append({'db': db["dbName"], 'result': 'Failed'})
+
+        # continue backing up next DB
+        continue
+
+print("Backup results:")
+print("--------------")
+for result in result_list:
+    print '{0:20} ==> {1:10}'.format(result["db"], result["result"])
